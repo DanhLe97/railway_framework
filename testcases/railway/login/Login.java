c65fd87b4257c3a4bb03cb975660766b844fc9e4
Danh Le <lcdanh.testing@gmail.com>
2023-09-06 00:26:36 +0700

    not ready

--- testcases/railway/login/Login.java
+++ testcases/railway/login/Login.java
@@ -11,6 +11,7 @@ import org.testng.annotations.Test;
 
 import commons.BasePage;
 import pageObject.HomePageObject;
+import pageObject.LoginPageObject;
 import pageObject.RegisterPageObject;
 
 public class Login extends BasePage {
@@ -19,6 +20,7 @@ public class Login extends BasePage {
 	private String projectPath = System.getProperty("user.dir");
 	private String emailAddress;
 	private HomePageObject homePage;
+	private LoginPageObject loginPage;
 	private RegisterPageObject registerPage;
 	private String firstName;
 	private String lastName;
@@ -27,23 +29,92 @@ public class Login extends BasePage {
 public void beforeClass() {
 		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 		driver = new FirefoxDriver();
-		registerPage = new LoginPageObject(driver);
+		homePage = new HomePageObject();
 //		basePage = BasePage.getBasePageObject();
 		firstName = "w_firstName";
 		lastName = "w_lastName";
 		password = "123456";
-		emailAddress = "afc" + generateFakeNumber() + "@gmail.vn";
+		emailAddress = "";
 
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get("https://demo.nopcommerce.com");
+		driver.get("http://saferailway.somee.com/Page/HomePage.cshtml");
+		homePage.clickToRegisterPage();
+		registerPage = new RegisterPageObject();
+		registerPage.inputToEmailTextbox();
+		registerPage.inputToPasswordTextbox();
+		registerPage.inputToConfirmPasswordTextbox();
+		registerPage.inputToPassportNumber();
+		registerPage.clickToRegisterButton
+		
 	  }
   @Test
   public void TC_01_Login_Valid_Username_Password() {
-	  
+	  	homePage.clickToLoginPage();
+	  	loginPage = new LoginPageObject();
+	  	loginPage.inputToEmailTextbox(emailAddress);
+	  	loginPage.inputToPasswordTextbox(password);
+	  	loginPage.clickToLoginButton();
+	  	
+	  	
   }
   @Test
-  public void TC_02() {
+  public void TC_02_Login_Blank_Username() {
+	  homePage.clickToLoginPage();
+	  loginPage = new LoginPageObject();
+	  	loginPage.inputToEmailTextbox("");
+	  	loginPage.inputToPasswordTextbox(password);
+	  	loginPage.clickToLoginButton();
+	  	
+	  	
+  }
+  @Test
+  public void TC_03_Login_Invalid_Password() {
+	  homePage.clickToLoginPage();
+
+	  loginPage = new LoginPageObject();
+	  	loginPage.inputToEmailTextbox(emailAddress);
+	  	loginPage.inputToPasswordTextbox("111111");
+	  	loginPage.clickToLoginButton();
+	  	
+	  	
+  }
+  @Test
+  public void TC_04_Login_Wrong_Password_More_Than_3_Times() {
+	  homePage.clickToLoginPage();	  
+	  int i=0;
+	  
+	  do {
+		 
+		  loginPage = new LoginPageObject();
+		  	loginPage.inputToEmailTextbox(emailAddress);
+		  	loginPage.inputToPasswordTextbox("");
+		  	loginPage.clickToLoginButton();
+		  	i++;
+	} while (i<4);
+	  
+	  	
+	  	
+  }
+  @Test
+  public void TC_05_Login_Inactive_Account() {
+	  homePage.clickToRegisterPage();
+		registerPage = new RegisterPageObject();
+		
+		registerPage.inputToEmailTextbox();
+		registerPage.inputToPasswordTextbox();
+		registerPage.inputToConfirmPasswordTextbox();
+		registerPage.inputToPassportNumber();
+		registerPage.clickToRegisterButton();
+		homePage.clickToLoginPage();
+		
+
+	  loginPage = new LoginPageObject();
+	  	loginPage.inputToEmailTextbox(emailAddress);
+	  	loginPage.inputToPasswordTextbox("111111");
+	  	loginPage.clickToLoginButton();
+	  	
+	  	
   }
 	public int generateFakeNumber() {
 		Random rand = new Random();
